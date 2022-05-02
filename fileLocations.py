bae63a8daf4af77e97e4e86d109ed028e14a2b6d
Kmass7 <104742470+Kmass7@users.noreply.github.com>
2022-05-01 22:02:14 -0500

    Create fileLocations.py

---
+++ fileLocations.py
@@ -0,0 +1,4 @@
+outputPath = r"C:\Users\user1\Desktop\My_Stuff\Books"
+srcPath = r"C:\Users\user1\Desktop\My_Stuff\Books\kindle-notes-to-excel"
+myClippingsPath = r"G:\documents\My Clippings.txt"
+vbaProjectPath = r"C:\Users\user1\AppData\Local\Programs\Python\Python310\Scripts\vbaProject.bin"
